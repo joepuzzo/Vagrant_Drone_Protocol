9cecf974b07c67be6401b4fe6b93f2a7d3889a25
joepuzzo <jgpuzzo@metrocast.net>
2016-02-23 19:47:38 -0500

    Matt fixed 0 based error

--- vagrant_core/assets/launchcore.sh
+++ vagrant_core/assets/launchcore.sh
@@ -1,9 +1,8 @@
-coresendmsg node flags=add number=0 name='LanWorld' type=6 model='WLAN' xpos=100 ypos=100 -s $1
-coresendmsg node flags=add number=1 name='d1' model='drone' xpos=200 ypos=200 -s $1
-coresendmsg node flags=add number=2 name='d2' model='drone' xpos=200 ypos=100 -s $1
-coresendmsg node flags=add number=3 name='d3' model='drone' xpos=100 ypos=200 -s $1
+coresendmsg node flags=add number=1 type=6 name='LanWorld' xpos=100 ypos=100 -s $1
+coresendmsg node flags=add number=2 type=0 name='d1' model='drone' xpos=200 ypos=200 -s $1
+coresendmsg node flags=add number=3 type=0 name='d2' model='drone' xpos=200 ypos=100 -s $1
+coresendmsg node flags=add number=4 type=0 name='d3' model='drone' xpos=100 ypos=200 -s $1
 
-coresendmsg link flags=add n1number=1 n2number=0 -s $1
-coresendmsg link flags=add n1number=2 n2number=0 -s $1
-coresendmsg link flags=add n1number=3 n2number=0 -s $1
-
+coresendmsg link flags=add n1number=2 n2number=1 -s $1
+coresendmsg link flags=add n1number=3 n2number=1 -s $1
+coresendmsg link flags=add n1number=4 n2number=1 -s $1
